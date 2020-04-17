06b2955b8e0023f462222a5084341215b4354cc1
victormongare1 <vicmongz254@gmail.com>
2020-04-17 21:37:35 +0300

    created a class for user

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+  '''
+  Class that generates intances of a user
+  '''
+  user_list=[]
+  def __init__(self,firstname,lastname):
+    self.firstname=firstname
+    self.lastname=lastname
+    
