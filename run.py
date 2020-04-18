a418e63eb5da11ffb4722d92ec236d455059b950
victormongare1 <vicmongz254@gmail.com>
2020-04-18 17:58:36 +0300

    created functions to add new user and credentials

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3.6
 from user import User
 from credentials import Credentials
+def create_user(accountname,accountpassword):
+  '''
+  function to create a new contact 
+  '''
+  newuser = User(accountname,accountpassword)
+  return newuser
+def create_credentials(website,username,password):
+  '''
+  function to create a new credentials 
+  '''
+  new_credentials = Credentials(website,username,password)
+  return new_credentials 
