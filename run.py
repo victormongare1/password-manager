c31af315ca53b25ff9e69ab9f95724b68bcc3455
victormongare1 <vicmongz254@gmail.com>
2020-04-19 22:51:27 +0300

    implemented function to remove credentials

--- run.py
+++ run.py
@@ -57,84 +57,139 @@ def main():
   print("Are you a registered user ? y/n type q to quit")   
   status="" 
   status=input()   
-  user={}
+  user={'name':'',"password":""}
   
   def newUser():
     print("create username")
     newlogin=input()
-    if newlogin in user:
-      print("username already exists")
-    else:
-      print("Create login password")
-      newpassword=input()
-      user[newlogin]=newpassword
-      print("Account created")                   
+    print("create password")
+    newpassword=input()
+    save_user(create_user(newlogin,newpassword))
+    with open('account1.csv','w') as f:
+      writer=csv.writer(f)
+      writer.writerow([f"{newlogin}",f"{newpassword}"])    
+    print ("account created")             
   if status == "y":
-      print("Enter your username")
-      login=input()
-      print("Enter password")
-      password=input()
-      if login in user and user[login]==password:
-        print("login successful")
-      else :
-        print("wrong password or user does not exist")
-  elif status == "n":
-      newUser()
-  else  :
-    print("exit")
-      
-
-  while True:
+    print("Enter your username")
+    login=input()
+    print("Enter password")
+    password=input()
+    with open('account1.csv','r') as f:
+      reader=csv.reader(f)
+      for account in reader:
+        if login in account[0] and password in account[1]:
+          print("login successful")
     
-    print('''Use the following codes to navigate through the application
-      1.Use dc to display credentials.
-      2.Use cc to create credentials.
-      3.Use rm to remove credentials.
-      4.Use ex to exit the application''')
-    short_code=input().lower()  
-    if short_code=="cc": 
-      print("                           NEW CREDENTIALS")
-      print("-"*80)
-      print("Enter website name :")
-      website=input()
-      print("Enter website username :")
-      websiteusername=input()
-      print("Enter Website password :")
-      websitepassword=input()
-      save_credentials(create_credentials(website,websiteusername,websitepassword))
-      print('\n')
-      print(f'''You have added 
-      Website name :{website}
-      Username :{websiteusername}
-      Password :{websitepassword} to your credentials''')
-      with open('account1.csv','a') as f:
-        writer=csv.writer(f)
-        writer.writerow([f'{website}',f'{websiteusername}',f'{websitepassword}'])
-      print('\n')
-    elif short_code=="dc":
-      if display_credentials:
-        print("These are all your usernames and credentials :" )
-        print("\n")
-        counter= 0
-        for credential in display_credentials():
-          counter+=1
-          print(f"{counter}.{website}...{websiteusername}...{websitepassword}")
-          print("\n")
-      else:
-        print("\n")
-        print("You do not have any credentials ,type cc to add credentials")
-        print("\n")
-    elif short_code=="rm":
-        print("Enter website of credential you want to remove:")
-        credentials=input()
-        cred  = check_existing_credentials(credentials)
-        del_credentials(cred)
-        print("credentials for website "f"{credentials}"" have been removed")
-  
-    elif short_code=="ex":
-      print("You have exited from the app,bye............")
-      break
-    else:
-      print("COMMAND NOT FOUND,PLEASE USE THE PROVIDED CODES")     
+          while True:
+          
+            print('''Use the following codes to navigate through the application
+              1.Use dc to display credentials.
+              2.Use cc to create credentials with your own passwords.
+              3.Use rm to remove credentials.
+              4.Use ex to exit the application''')
+            short_code=input().lower()  
+            if short_code=="cc": 
+              print("                           NEW CREDENTIALS")
+              print("-"*80)
+              print("Enter website name :")
+              website=input()
+              print("Enter website username :")
+              websiteusername=input()
+              print("Enter Website password :")
+              websitepassword=input()
+              save_credentials(create_credentials(website,websiteusername,websitepassword))
+              print('\n')
+              print(f'''You have added 
+              Website name :{website}
+              Username :{websiteusername}
+              Password :{websitepassword} to your credentials''')
+              with open('account1.csv','a') as f:
+                writer=csv.writer(f)
+                writer.writerow([f'{website}',f'{websiteusername}',f'{websitepassword}'])
+              print('\n')
+            elif short_code=="dc":
+              if display_credentials:
+                print("These are all your usernames and credentials :" )
+                print("\n")
+                counter= 0
+                for credential in display_credentials():
+                  counter+=1
+                  print(f"{counter}.{website}...{websiteusername}...{websitepassword}")
+                  print("\n")
+              else:
+                print("\n")
+                print("You do not have any credentials ,type cc to add credentials")
+                print("\n")
+            elif short_code=="rm":
+                print("Enter website of credential you want to remove:")
+                credentials=input()
+                cred  = check_existing_credentials(credentials)
+                del_credentials(cred)
+                print("credentials for website "f"{credentials}"" have been removed")
+          
+            elif short_code=="ex":
+              print("You have exited from the app,bye............")
+              break
+            else:
+              print("COMMAND NOT FOUND,PLEASE USE THE PROVIDED CODES")
+        else:
+          print("account not found or wrong password")
+    
+  elif status == "n":
+    newUser()
+    while True:
+      
+        print('''Use the following codes to navigate through the application
+          1.Use dc to display credentials.
+          2.Use cc to create credentials with your own passwords.
+          3.Use rm to remove credentials.
+          4.Use ex to exit the application''')
+        short_code=input().lower()  
+        if short_code=="cc": 
+          print("                           NEW CREDENTIALS")
+          print("-"*80)
+          print("Enter website name :")
+          website=input()
+          print("Enter website username :")
+          websiteusername=input()
+          print("Enter Website password :")
+          websitepassword=input()
+          save_credentials(create_credentials(website,websiteusername,websitepassword))
+          print('\n')
+          print(f'''You have added 
+          Website name :{website}
+          Username :{websiteusername}
+          Password :{websitepassword} to your credentials''')
+          with open('account1.csv','a') as f:
+            writer=csv.writer(f)
+            writer.writerow([f'{website}',f'{websiteusername}',f'{websitepassword}'])
+          print('\n')
+        elif short_code=="dc":
+          if display_credentials:
+            print("These are all your usernames and credentials :" )
+            print("\n")
+            counter= 0
+            for credential in display_credentials():
+              counter+=1
+              print(f"{counter}.{website}...{websiteusername}...{websitepassword}")
+              print("\n")
+          else:
+            print("\n")
+            print("You do not have any credentials ,type cc to add credentials")
+            print("\n")
+        elif short_code=="rm":
+            print("Enter website of credential you want to remove:")
+            credentials=input()
+            cred  = check_existing_credentials(credentials)
+            del_credentials(cred)
+            print("credentials for website "f"{credentials}"" have been removed")
+      
+        elif short_code=="ex":
+          print("You have exited from the app,bye............")
+          break
+        else:
+          print("COMMAND NOT FOUND,PLEASE USE THE PROVIDED CODES")
+  else  :
+    print("exit")   
 if __name__ == "__main__":
   main()
